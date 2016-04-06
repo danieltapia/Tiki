c4008f06884609585e63493de34bbfd6bdc0b1f8
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2016-04-06 10:00:56 -0400

    Add default header for header.jsx

--- imports/ui/layouts/Header.jsx
+++ imports/ui/layouts/Header.jsx
@@ -19,10 +19,28 @@ export default class Header extends React.Component {
     )
   }
 
+  defaultHeader() {
+    return (
+      <div className="nav-inner flex-box">
+        <div className="nav-left">
+          <a href="#" className="nav-icon"><i className="material-icons md-light">arrow_back</i></a>
+        </div>
+
+        <div className="nav-middle flex-item">
+          <div className="middle-text">This is the default header</div>
+        </div>
+
+        <div className="nav-right">
+          <a href="#" className="nav-icon"><i className="material-icons md-light">search</i></a>
+        </div>
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className="mobile-nav navbar-fixed-top">
-        { this.props.categorySlug ? this.categoryHeader() : "" }
+        { this.props.categorySlug ? this.categoryHeader() : this.defaultHeader() }
       </div>
     )
   }
