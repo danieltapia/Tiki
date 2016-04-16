634d30e447c726dbb958cc0625e6108de11fed92
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2016-04-16 19:46:22 -0400

    Add chat dummy page.

--- imports/client/routes.jsx
+++ imports/client/routes.jsx
@@ -6,10 +6,12 @@ import CategoryContainer from '/imports/ui/containers/CategoryContainer.jsx';
 import ListingContainer from '/imports/ui/containers/ListingContainer.jsx';
 import NewListingContainer from '/imports/ui/containers/NewListingContainer.jsx';
 import UserSettingsContainer from '/imports/ui/containers/UserSettingsContainer.jsx';
+import ChatContainer from '/imports/ui/containers/ChatContainer.jsx';
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={AppLayout}>
+      <Route path="chat" component={ChatContainer}/>
       <Route path="new-listing" component={NewListingContainer}/>
       <Route path="settings" component={UserSettingsContainer}/>
       <Route path="category/:categorySlug" component={CategoryContainer}/>
---
+++ imports/ui/containers/ChatContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ChatContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the chat container.</h1>
+        <h1>This is the chat container.</h1>
+      </div>
+    )
+  }
+}
