a215247dc8a1a1bfcc05e66684771d049fc2bef2
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2016-04-19 13:36:58 -0400

    Add login panel component.

--- imports/client/routes.jsx
+++ imports/client/routes.jsx
@@ -7,10 +7,12 @@ import ListingContainer from '/imports/ui/containers/ListingContainer.jsx';
 import NewListingContainer from '/imports/ui/containers/NewListingContainer.jsx';
 import UserSettingsContainer from '/imports/ui/containers/UserSettingsContainer.jsx';
 import ChatContainer from '/imports/ui/containers/ChatContainer.jsx';
+import Login from '/imports/ui/components/login/Login.jsx'
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={AppLayout}>
+      <Route path="login" component={Login} />
       <Route path="chat/:chatSlug" component={ChatContainer}/>
       <Route path="new-listing" component={NewListingContainer}/>
       <Route path="settings" component={UserSettingsContainer}/>
---
+++ imports/ui/components/login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <div id="login-panel">
+        <h1>This is the login panel.</h1>
+      </div>
+    )
+  }
+}
