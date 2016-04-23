bc253bbf3f7650b12562d33964b45bfeebfdf64b
Daniel Tapia <dan.tg@outlook.com>
2016-04-23 13:41:54 -0400

    Added login modal.

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import { renderRoutes } from '/imports/client/routes.jsx'
+import { renderRoutes } from '/imports/client/routes.jsx';
+import bootstrap from 'bootstrap';
 
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('app'));
--- imports/ui/components/login/Login.jsx
+++ imports/ui/components/login/Login.jsx
@@ -3,12 +3,28 @@ import React from 'react';
 export default class Login extends React.Component {
   render() {
     return (
-      <div id="login-panel">
-        <h1>DANIEL ADD THE LOGIN HERE</h1>
-        <h1>DANIEL ADD THE LOGIN HERE</h1>
-        <h1>DANIEL ADD THE LOGIN HERE</h1>
-        <h1>DANIEL ADD THE LOGIN HERE</h1>
-        <h1>DANIEL ADD THE LOGIN HERE</h1>
+      <div className="login-page">
+        
+        <button className="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Launch demo modal</button>
+
+        <div className="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div className="vertical-alignment-helper">
+            <div className="modal-dialog vertical-align-center">
+              <div className="modal-content">
+
+                <div className="modal-body text-center">
+                  <h4>Inicia sesión</h4>
+                  
+                  <p className="subtle-text">Oops! Necesitas iniciar sesión para <br/> poder utilizar todos los servicios que brinda Tiki.</p>
+                  
+                  <a href="#" className="btn btn-block btn-facebook">Conéctate con Facebook</a>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     )
   }
